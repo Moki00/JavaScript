07b72fed5ed41cf65e213cedec1d8fa555bde277
MoKi <61506669+moki929@users.noreply.github.com>
2020-10-29 09:13:08 -0400

    almost done with DataStructures

--- DataStructures.js
+++ DataStructures.js
@@ -333,3 +333,29 @@ console.log(getArrayOfUsers(users));
 //*+-*-+*+-*+*+*+*+-+
 console.clear(); // to clear the browser console ***CLEAR***
 //*+-*-+*+-*+*+*+*+-+
+
+let user = {
+    name: "Kenneth",
+    age: 28,
+    data: {
+        username: "kennethCodesAllDay",
+        joinDate: "March 26, 2016",
+        organization: "freeCodeCamp",
+        friends: ["Sam", "Kira", "Tomo"],
+        location: {
+            city: "San Francisco",
+            state: "CA",
+            country: "USA",
+        },
+    },
+};
+
+function addFriend(userObj, friend) {
+    // Only change code below this line
+    console.log(userObj);
+    console.log(friend);
+
+    // Only change code above this line
+}
+
+console.log(addFriend(user, "Pete"));
