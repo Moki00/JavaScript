6c423721c789bda6a350c84692eb1d9065fac390
MoKi <61506669+moki929@users.noreply.github.com>
2020-11-23 21:26:05 -0500

    it works!

--- telephone.js
+++ telephone.js
@@ -1,55 +1,55 @@
 function telephoneCheck(str) {
-    str = "(545)555-5555";
     // easy to decipher and generate
-    //create small regex tests for each variable
-    let nondigits = /\D/g;
-    let digitCount = str.replaceAll(nondigits, "").length;
-    console.log(digitCount);
+    let dash2 = /^\d{3}-\d{3}-\d{4}$/gm; // 123-555-7890
+    let spaces = /^\d{3}\s*\d{3}\s*\d{4}$/gm; // 555 555 7890 and 0123456789
+    let space1 = /^1\s*\d{3}\s*\d{3}\s*\d{4}$/gm; // 1 234 555 8901
+    let dash2With1 = /^1\s*\d{3}-\d{3}-\d{4}$/gm; // 1 234-555-8901
+    let bracketSpaceDash1 = /^(1[\s-]?)?[(]\d{3}[)]\s*\d{3}-\d{4}$/gm; // 1 (555) 555-5575 and (555) 555-7890 and (555)555-7890
 
-    let one = /^1/g;
-    let startsWithOne = str.match(one);
-    // console.log(startsWithOne);
+    let format1 = str.match(dash2);
+    let format3 = str.match(spaces);
+    let format4 = str.match(space1);
+    let format5 = str.match(dash2With1);
+    let format6 = str.match(bracketSpaceDash1);
 
-    let dash2 = /\d{3}-\d{3}-\d{4}/g; // 555-555-5555
-    let bracketDash = /[(]\d{3}[)]\d{3}-\d{4}/g; // (555)555-5555
-    // (555) 555-5555
-    // 555 555 5555
-    // 5555555555
-    // 1 555 555 5555
+    console.log(format1 + " has 2 dashes");
+    console.log(format3 + " has 0-2 spaces");
+    console.log(format4 + " has a 1 with spaces");
+    console.log(format5 + " has a 1 with 2 dashes");
+    console.log(format6 + " has brackets and a dash and maybe 1");
 
-    let formats = str.match("(/[(]d{3}[)]d{3}-d{4}/g) | (/d{3}-d{3}-d{4}/g)");
-
-    console.log(formats);
-
-    let hasCorrectParentheses = true;
-
-    // validate each format from the example per new line
-
-    // examples that should not be selected
-    if (digitCount < 10 || digitCount > 11) {
-        return false;
-    } else if (!formats || !hasCorrectParentheses) {
-        return false;
-    } else if ((digitCount = 11 && !startsWithOne)) {
-        return false;
-    } else {
+    if (format1 || format3 || format4 || format5 || format6) {
         return true;
+    } else {
+        return false;
     }
 }
 
-// 7 true
+// 8 true
 console.log(telephoneCheck("545-555-5555"));
-// console.log(telephoneCheck("255 555 5555"));
-// console.log(telephoneCheck("5555553555"));
-// console.log(telephoneCheck("1 (555) 595-5555"));
-// console.log(telephoneCheck("(555)555-5558"));
-// console.log(telephoneCheck("(555) 556-5555"));
-// console.log(telephoneCheck("1 555-555-5575"));
+console.log(telephoneCheck("255 555 5555"));
+console.log(telephoneCheck("5555553555"));
+console.log(telephoneCheck("1 (555) 595-5555")); // 4
+console.log(telephoneCheck("(555)555-5558"));
+console.log(telephoneCheck("(555) 556-5555"));
+console.log(telephoneCheck("1 555-555-5575"));
+console.log(telephoneCheck("1 (555) 555-5575")); // 8
 
-// // 6 false
+// // 7 false
+// console.log(telephoneCheck("2 (757) 622-7382"));
 // console.log(telephoneCheck("555)555-5955")); //doesn't -->hasCorrectParentheses
 // console.log(telephoneCheck("5egge4eg55")); //doesn't -->hasPermittedCharsOnly
 // console.log(telephoneCheck("-1 555-555-5755")); //has a minus
 // console.log(telephoneCheck("2 555-565-5555")); //11 digits doesn't -->startsWithOne
 // console.log(telephoneCheck("555-555-55595")); //11 digits doesn't -->startsWithOne
 // console.log(telephoneCheck("555-555-558")); //9 digits -->hasTenDigits
+
+//create small regex tests for each variable
+let exampleTelephone = "470-272-0054";
+let nondigits = /\D/g;
+let digitCount = exampleTelephone.replaceAll(nondigits, "").length;
+console.log(digitCount);
+
+let one = /^1/g;
+let startsWithOne = exampleTelephone.match(one);
+console.log(startsWithOne);
